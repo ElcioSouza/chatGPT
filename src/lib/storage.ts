c22ccf31703da99a4594f1eb2a70d06b85f6388a
unknown <elcio.monico@gmail.com>
2023-08-04 14:55:28 -0300

    ajuste

--- src/lib/storage.ts
+++
@@ -1,9 +0,0 @@
-const setItem = (key: string, value: string) => {
-  sessionStorage.setItem(key, value);
-};
-
-const getItem = (key: string) => typeof window !== 'undefined' ? sessionStorage.getItem(key) : undefined;
-
-const removeItem = (key: string) => sessionStorage.removeItem(key);
-
-export { setItem, getItem, removeItem };
