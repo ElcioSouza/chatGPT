749097acd0e5d004ad03dca60626332a6fb220f9
unknown <elcio.monico@gmail.com>
2023-08-04 15:54:54 -0300

    ajuste

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,6 @@
 import { NextResponse,NextRequest } from "next/server";
 
 export function middleware(request: NextRequest,res:NextResponse) {
-   console.log("middleare")     
-  //console.log(NextRequest.prototype)
    let cookie = request.cookies.get('token')
    if(!cookie) {
       return NextResponse.redirect(new URL("/login",  request.url))
